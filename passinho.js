672d3a96257b70f83cdb928cc832315ebc90841c
Daniel Bennesby <danielbennesby@gmail.com>
2022-02-24 01:19:49 -0300

    add logica base64

--- passinho.js
+++ passinho.js
@@ -1,13 +1,45 @@
-function print(text) {
-    var texto = document.getElementById("texto");
-    var base64 = document.getElementById("base64");
-    var cesar = document.getElementById("cesar");
-    console.log(texto.value);
+
+let texto = document.getElementById("texto")
+let base = document.getElementById("base64")
+let cesar = document.getElementById("cesar")
+let quant = document.getElementById("quantidade")
+let botao = document.getElementById("botao")
+let codificar = document.getElementById("codificar")
+let decodificar = document.getElementById("decodificar")
+
+
+
+cesar.addEventListener("click", function(){
+  quant.style.display= "block"
   
-    if (document.getElementById("base64").checked) {
-          var base64Codificado = btoa(texto.value);
-      console.log(base64Codificado);
-      var base64Decodificado = atob(base64Codificado);
-      console.log(base64Decodificado);
-    }
+})
+
+base.addEventListener("click", function(){
+  quant.style.display= "none"
+
+})
+
+botao.addEventListener("click", function(elemento){
+ elemento.preventDefault()
+if(base.checked){
+  base64()
+} else if(cesar.checked){
+
+
+}
+
+})
+
+function base64 (){
+  let txt = texto.value
+  if(decodificar.checked){
+    let de=atob(txt)
+    alert(de)
+
+  }else if(codificar.checked){
+    let co = btoa(txt)
+    alert (co)
+
   }
+
+}
